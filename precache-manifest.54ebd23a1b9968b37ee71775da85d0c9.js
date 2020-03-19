f7c3217590c14ade0874b91bbd4b5bb561101e8a
Matthew <52917277+matthewcalcagno@users.noreply.github.com>
2020-03-19 11:51:35 +0100

    Updates

---
+++ precache-manifest.54ebd23a1b9968b37ee71775da85d0c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1c1ca25e66aff1a56824d50a92e5d8",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f139aefbd1df7b94280b",
+    "url": "/covid19/static/js/2.df0e262b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19/static/js/2.df0e262b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2238eb84e3a18376c5ef",
+    "url": "/covid19/static/js/main.560b7259.chunk.js"
+  },
+  {
+    "revision": "93c446c5dee05bc4619c",
+    "url": "/covid19/static/js/runtime-main.a45f89ed.js"
+  }
+]);
